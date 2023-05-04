72d2cf90947a729882f32af95117b7ee9f67e816
vusalaxndzde <vusal.axndzde@gmail.com>
2023-05-04 18:28:33 +0400

    changes

--- src/main/java/com/bakulibrary/library/entity/Book.java
+++ src/main/java/com/bakulibrary/library/entity/Book.java
@@ -1,9 +1,15 @@
 package com.bakulibrary.library.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "book")
 public class Book {
--- src/main/java/com/bakulibrary/library/entity/Genre.java
+++ src/main/java/com/bakulibrary/library/entity/Genre.java
@@ -1,9 +1,15 @@
 package com.bakulibrary.library.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "genre")
 public class Genre {
@@ -17,5 +23,13 @@ public class Genre {
 
     @ManyToMany(mappedBy = "genres")
     private List<Book> books;
+
+    @Override
+    public String toString() {
+        return "Genre{" +
+                "id=" + id +
+                ", genreName='" + genreName + '\'' +
+                '}';
+    }
 
 }
