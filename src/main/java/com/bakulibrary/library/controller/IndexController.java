110534e91330db8768198d2bc42b7c3cd8e6ebb1
vusalaxndzde <vusal.axndzde@gmail.com>
2023-05-04 20:25:46 +0400

    changes

--- src/main/java/com/bakulibrary/library/controller/IndexController.java
+++ src/main/java/com/bakulibrary/library/controller/IndexController.java
@@ -1,10 +1,14 @@
 package com.bakulibrary.library.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
-
+    @GetMapping("/")
+    public String showIndex() {
+        return "index";
+    }
 
 }
