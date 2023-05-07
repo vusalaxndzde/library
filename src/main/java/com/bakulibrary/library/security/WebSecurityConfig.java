92772798b597a589be95b3902db1f91722f905f0
vusalaxndzde <vusal.axndzde@gmail.com>
2023-05-07 22:19:27 +0400

    changes

--- src/main/java/com/bakulibrary/library/security/WebSecurityConfig.java
+++ src/main/java/com/bakulibrary/library/security/WebSecurityConfig.java
@@ -42,7 +42,7 @@ public class WebSecurityConfig {
                             .requestMatchers("/registration/**", "/login/**").permitAll()
                             .requestMatchers("/css/**", "/image/**", "/js/**").permitAll()
                             .requestMatchers("/test").hasRole("ADMIN")
-                            .anyRequest().authenticated()
+                            .anyRequest().permitAll()
                 )
 
                 .formLogin(form ->
