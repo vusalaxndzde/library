fb5f1c805607da8569a803489c897059d343a036
vusalaxndzde <vusal.axndzde@gmail.com>
2023-05-07 21:34:53 +0400

    changes

--- src/main/java/com/bakulibrary/library/controller/ErrorControllerImpl.java
+++ src/main/java/com/bakulibrary/library/controller/ErrorControllerImpl.java
@@ -1,4 +1,27 @@
 package com.bakulibrary.library.controller;
 
-public class ErrorControllerImpl {
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ErrorControllerImpl implements ErrorController {
+
+    @GetMapping("/error")
+    public String showErrorPage(HttpServletRequest request, Model model) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        String url = (String) request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+            model.addAttribute("status", statusCode);
+        }
+        model.addAttribute("url", url);
+
+        return "error";
+    }
+
 }
