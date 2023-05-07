d6e95890fe69c0614153137bff0c45c052f8c7d9
vusalaxndzde <vusal.axndzde@gmail.com>
2023-05-07 21:45:25 +0400

    changes

--- src/main/java/com/bakulibrary/library/controller/ErrorControllerImpl.java
+++ src/main/java/com/bakulibrary/library/controller/ErrorControllerImpl.java
@@ -14,12 +14,16 @@ public class ErrorControllerImpl implements ErrorController {
     public String showErrorPage(HttpServletRequest request, Model model) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         String url = (String) request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI);
+        String msgHead = "Look like you're lost";
+        String msg = "the page you are looking for not available!";
 
         if (status != null) {
             int statusCode = Integer.parseInt(status.toString());
             model.addAttribute("status", statusCode);
         }
         model.addAttribute("url", url);
+        model.addAttribute("messageHead", msgHead);
+        model.addAttribute("message", msg);
 
         return "error";
     }
