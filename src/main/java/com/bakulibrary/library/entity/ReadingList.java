c5098003d3df5ee8c37ef8416b0acaa8803feaf6
vusalaxndzde <vusal.axndzde@gmail.com>
2023-05-09 01:58:25 +0400

    changes

--- src/main/java/com/bakulibrary/library/entity/ReadingList.java
+++ src/main/java/com/bakulibrary/library/entity/ReadingList.java
@@ -1,7 +1,13 @@
 package com.bakulibrary.library.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "reading_list")
 public class ReadingList {
