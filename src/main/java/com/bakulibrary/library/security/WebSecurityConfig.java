7ac57599deda9d753ae871181448fe3f7b87db2e
vusalaxndzde <vusal.axndzde@gmail.com>
2023-05-09 02:58:32 +0400

    changes

--- src/main/java/com/bakulibrary/library/security/WebSecurityConfig.java
+++ src/main/java/com/bakulibrary/library/security/WebSecurityConfig.java
@@ -42,6 +42,7 @@ public class WebSecurityConfig {
                             .requestMatchers("/registration/**", "/login/**").permitAll()
                             .requestMatchers("/css/**", "/image/**", "/js/**").permitAll()
                             .requestMatchers("/test").hasRole("ADMIN")
+                            .requestMatchers("/account/books").hasRole("USER")
                             .anyRequest().permitAll()
                 )
 
@@ -55,7 +56,7 @@ public class WebSecurityConfig {
 
                 .logout(logout ->
                     logout
-                            .logoutSuccessUrl("/")
+//                            .logoutSuccessUrl("/")
                             .permitAll()
                 );
 
--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,6 +1,3 @@
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 let map;
 let service;
 let infowindow;
