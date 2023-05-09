2478c5fc71c384c699f68a5dc9a04b1bc1d3a9d3
vusalaxndzde <vusal.axndzde@gmail.com>
2023-05-09 16:50:17 +0400

    changes

---
+++ src/main/java/com/bakulibrary/library/repository/ReadingListRepository.java
@@ -0,0 +1,10 @@
+package com.bakulibrary.library.repository;
+
+import com.bakulibrary.library.entity.ReadingList;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReadingListRepository extends JpaRepository<ReadingList, Integer> {
+
+
+
+}
