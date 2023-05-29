469058ba852b3665419b3acc9ddbfe0d5d87e56d
vusalaxndzde <vusal.axndzde@gmail.com>
2023-05-29 20:40:42 +0400

    changes

--- src/main/java/com/bakulibrary/library/controller/UserController.java
+++ src/main/java/com/bakulibrary/library/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.bakulibrary.library.controller;
 
+import com.bakulibrary.library.dto.UpdatePasswordDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,8 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     @GetMapping
-    public String showUserDetails() {
+    public String showUserDetails(Model model) {
+        model.addAttribute("updatePassword", new UpdatePasswordDTO());
         return "user-details";
+    }
+
+    @PostMapping("/updatePassword")
+    public String updatePassword(@ModelAttribute("updatePassword") UpdatePasswordDTO updatePasswordDTO) {
+
+        return "redirect:/account";
     }
 
 }
---
+++ src/main/java/com/bakulibrary/library/dto/UpdatePasswordDTO.java
@@ -0,0 +1,18 @@
+package com.bakulibrary.library.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdatePasswordDTO {
+
+    private String currentPassword;
+
+    private String newPassword;
+
+    private String confirmNewPassword;
+
+}
