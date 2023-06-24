41dced2bc4726f09335b6458a70d6a9d6691cb09
vusalaxndzde <vusal.axndzde@gmail.com>
2023-06-24 22:36:23 +0400

    changes

--- src/main/java/com/bakulibrary/library/LibraryApplication.java
+++ src/main/java/com/bakulibrary/library/LibraryApplication.java
@@ -2,7 +2,10 @@ package com.bakulibrary.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
@@ -11,6 +14,11 @@ public class LibraryApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+	@Bean
+	public ServletWebServerFactory servletWebServerFactory() {
+		return new TomcatServletWebServerFactory();
 	}
 
 }
