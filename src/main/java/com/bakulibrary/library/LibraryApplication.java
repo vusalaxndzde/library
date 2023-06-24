8950a6bd11aa74e379499efd2728b105b827d26c
vusalaxndzde <vusal.axndzde@gmail.com>
2023-06-24 23:20:39 +0400

    changes

--- src/main/java/com/bakulibrary/library/LibraryApplication.java
+++ src/main/java/com/bakulibrary/library/LibraryApplication.java
@@ -2,10 +2,8 @@ package com.bakulibrary.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
-@Profile({"prod", "dev"})
 public class LibraryApplication {
 
 	public static void main(String[] args) {
